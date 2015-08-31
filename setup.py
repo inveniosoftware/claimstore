a6d351973df8c607835b0b8b63c3d4aa8b961dd2
Tibor Simko <tibor.simko@cern.ch>
2015-08-31 18:14:18 +0200

    global: GNU GPLv3 license update
    * Updates license version to GNU GPLv3.
    
    * Amends `docs/conf.py` WRT import path for autodoc.  (addresses #36)
    
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 #
 # ClaimStore is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
 # ClaimStore is distributed in the hope that it will be useful, but
@@ -110,7 +110,7 @@ setup(
         'Development Status :: 1 - Planning',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
